fbe44d86e3289a53ff7e9abd02ed4f8440632e44
liors <lior@billguard.com>
2013-07-08 17:05:21 +0300

    cleanup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,28 @@
 function TeamCtrl($scope) {
-	var yaron = { "name" : "Yaron Samid", "job" : "Founder & CEO", "img" : "yaronsamid"};
-	var raphael = { "name" : "Raphael Ouzan", "job": "Founder & CTO", "img" : "raphael"};
-  	var users = [yaron, raphael];
-  	$scope.users = users;    
+    var yaron = { "name" : "Yaron Samid", "job" : "Founder & CEO", "img" : "yaronsamid"};
+    var raphael = { "name" : "Raphael Ouzan", "job": "Founder & CTO", "img" : "raphael"};
+    var users = [yaron, raphael];
+    $scope.users = users;    
 }
 
 function FormCtrl($scope, $element, $http) {
-	$scope.submit = function() {
+    $scope.submit = function() {
         var url = "https://billguard.com/office-visitors?"+$element.serialize()+"&callback=JSON_CALLBACK";
         $http.jsonp(url).success(function(data){
-        	if (data.status === 'OK') {
-				resetForm();
-        		window.bootbox.alert("Thanks!");        		
-			} else {
-				window.alert("Oops something went wrong...");
-			}
+            if (data.status === 'OK') {
+                resetForm();
+                window.bootbox.alert("Thanks!");                
+            } else {
+                window.alert("Oops something went wrong...");
+            }
        });   
-  	};
-
-  	function resetForm() {
-    	$scope.firstName = "";
-		$scope.lastName = "";
-		$scope.company = "";
-		$scope.visiting = "";
     };
-	
+
+    function resetForm() {
+        $scope.firstName = "";
+        $scope.lastName = "";
+        $scope.company = "";
+        $scope.visiting = "";
+    };
+    
 }
